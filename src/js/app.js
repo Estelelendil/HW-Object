320f7c09edb0e15e129f9c806ac1f89c5db2fd26
Чернова Александра <estelelendil@mail.ru>
2022-07-25 14:11:22 +0300

    added new code, but not tasting

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,22 @@
+// const condition = ["name", "level"];
+// const result = [];
 
-const condition = ["name", "level"];
-const result = [];
-
-const objSopted = Object.fromEntries(Object.entries(obj).sort());//преобразует список пар ключ-значение в объект и сразу сортирует его
-for (const key in objSopted){
-    if (condition.includes(key)){
-        const index = condition.findIndex((item) => item ===key);// находит индекс ключа в образцовом массиве
-        result[index] = {key ,value: objSopted[key]};
-    }
-}
-for( const key in objSopted){
-    if(!condition.includes(key)){
-        result[result.length] = { key, value: objSopted[key]};
-    }
-    return result;
-}
+// const objSopted = Object.fromEntries(Object.entries(obj).sort());
+// for (const key in objSopted){
+//     if (condition.includes(key)){
+//         const index = condition.findIndex((item) => item ===key);
+//         result[index] = {key ,value: objSopted[key]};
+//     }
+// }
+// for( const key in objSopted){
+//     if(!condition.includes(key)){
+//         result[result.length] = { key, value: objSopted[key]};
+//     }
+//     // return result;
+// }
+function compare(a, b) {
+    if(a.key.firstname < b.key.firstname) { return -1; };
+    if(a.key.firstname > b.key.firstname) { return 1; };
+    return 0;
+};
+export default compare;
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,16 +1,18 @@
-let obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40};
+/* eslint-disable guard-for-in */
+const obj = {
+  name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+};
 
-const orderByProps = function (object, arr) {
+const orderByProps = (object, arr) => {
   const newArr = [];
   for (const prop in object) {
-    arr.map((elem) => {
-      if (elem == prop) {
-        newArr.push({ key : obj.prop });
+    arr.forEach((elem) => {
+      if (elem === prop) {
+        newArr.push({ key: object.prop });
       }
-      return newArr;
     });
-    
   }
+
   return newArr;
 };
 console.log(orderByProps(obj, ['name', 'level']));
